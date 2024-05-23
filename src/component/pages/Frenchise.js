e1383e41ac0fff9f4170da47d20cef55066a3b5a
medcloud <medcloud.pk@gmail.com>
2024-05-23 18:12:33 +0500

    commit

--- src/component/pages/Frenchise.js
+++ src/component/pages/Frenchise.js
@@ -6,6 +6,9 @@ import { BsGraphUpArrow } from "react-icons/bs";
 import { FaPoundSign, FaChartLine } from 'react-icons/fa';
 import investment from '../../images/cost-of-kumon-franchise.png'
 import earnings from '../../images/profit+transparent.png'
+import kumon from '../../images/kumon-instructors.jpg'
+
+
 
 const Frenchise = () => {
 
@@ -146,19 +149,19 @@ const Frenchise = () => {
 
           <table
             className=""
-            align="center"
-            border={2}
-            bordercolor="#7dcdf4"
-            cellPadding={3}
-            cellSpacing={3}
+            // align="center"
+            // border={2}
+            // bordercolor="#7dcdf4"
+            // cellPadding={3}
+            // cellSpacing={3}
           >
             <tbody>
               <tr    className="px-[15px]"  >
                 <td>
-                  <h3 style={{ marginLeft: 15, marginRight: 15 }}   className=" font-[400]   text-[30px]     "   >
+                  <h3   className=" font-[400]  text-[22px] lg:text-[30px]     "   >
                     What does it mean to be a Kumon franchisee?
                   </h3>
-                  <p style={{ marginLeft: 15, marginRight: 15 }}>
+                  <p >
                     Becoming a Kumon franchisee provides a unique opportunity to
                     develop students through the Kumon programmes, as well as
                     building your own study centre and business.
@@ -227,8 +230,8 @@ const Frenchise = () => {
     <div className="row">
       <div className="col col-sm-12">
         <div className="wysiwyg_widget    text-l   mt-5   ">
-          <h2 className="text-center font-[400]  text-[3rem]">How much will it cost?</h2>
-          <p className="text-center text-[#666]   text-[22px]   mt-5 ">Explore the content below to learn more about the costs and rewards of owning your own Kumon centre.</p>
+          <h2 className="text-center font-[400] text-[2rem] md:text-[3rem]">How much will it cost?</h2>
+          <p className="text-center text-[#666]  text-[18px]  px-7 md:text-[22px]   mt-5 ">Explore the content below to learn more about the costs and rewards of owning your own Kumon centre.</p>
         </div>                              </div>
     </div>
   </div>
@@ -251,13 +254,13 @@ const Frenchise = () => {
 
 
     <div className=" mx-auto p-4     bg-[#f8f8f8]">
-        <div className="w-[80vw] py-[2rem] mx-auto">
+        <div className="     w-full  lg:w-[80vw] py-[2rem] mx-auto">
 
        
-      <div className="flex justify-center   ">
-        <ul className="flex space-x-10">
+      <div className="       w-[100%]   flex justify-center   ">
+        <ul className="  block    space-y-10 md:flex   md:space-y-0    space-x-0 md:space-x-10">
           <li
-            className={`cursor-pointer p-2 px-[3.5rem] pt-[1.8rem]  border-2  ${activeTab === 'investment' ? 'text-white bg-[#8cc242]  px-[3.5rem] pt-[1.5rem]  pb-[1rem]  border-2 block  border-[#8cc242]' : ' hover:bg-[#8cc242] hover:text-white   text-[#8cc242]   px-[3rem]  border-2  border-[#8cc242] pt-[1.5rem] pb-[1rem]  block '}`}
+            className={`cursor-pointer p-2    pt-[1.5rem]  border-2  ${activeTab === 'investment' ? 'text-white bg-[#8cc242]  px-[3.5rem] pt-[1.5rem]  pb-[1rem]  border-2 block  border-[#8cc242]' : ' hover:bg-[#8cc242] hover:text-white   text-[#8cc242]  px-[4rem] md:px-[3.5rem]  border-2  border-[#8cc242] pt-[1.5rem]   block '}`}
             onClick={() => handleTabClick('investment')}
           >
             <span className="mr-2  ">
@@ -267,7 +270,7 @@ const Frenchise = () => {
 
           </li>
           <li
-            className={`cursor-pointer p-2  px-[3.5rem] pt-[1.5rem] border-2  ${activeTab === 'earnings' ? ' text-white bg-[#8cc242]  px-[3.5rem] pt-[1.5rem]  pb-[1rem]  border-2 block  border-[#8cc242]' : '  hover:bg-[#8cc242] hover:text-white   text-[#8cc242]   px-[3rem]  border-2  border-[#8cc242] pt-[1.5rem] pb-[1rem]  block '}`}
+            className={`cursor-pointer p-2      pt-[1.8rem] border-2  ${activeTab === 'earnings' ? ' text-white bg-[#8cc242]  px-[3.5rem] pt-[1.5rem]  pb-[1rem]  border-2 block  border-[#8cc242]' : '  hover:bg-[#8cc242] hover:text-white   text-[#8cc242]  px-[4rem] md:px-[3.5rem]  border-2  border-[#8cc242] pt-[1.5rem] pb-[1rem]  block '}`}
             onClick={() => handleTabClick('earnings')}
           >
             <span className="mr-2   ">
@@ -278,18 +281,18 @@ const Frenchise = () => {
         </ul>
       </div>
 
-      <div className="mt-[3rem]  w-[75vw] ">
+      <div className="mt-[3rem]     w-full  md:w-[75vw] ">
         {activeTab === 'investment' && (
           <div className="flex flex-col md:flex-row items-center">
-            <div className="md:w-1/3">
+            <div className="  w-full   md:w-1/3    mb-[2rem] lg:mb-0  ">
               <img
                 src={investment}
                 alt="Cost of Kumon Franchise"
-                className="w-full h-auto"
+                className="w-full h-auto     "
               />
             </div>
-            <div className="md:w-2/3  md:mt-0 md:ml-4   px-3  ">
-              <ul className="list-disc list-inside  px-[3rem] text-xl space-y-8">
+            <div className="md:w-2/3  md:mt-0 md:ml-4  px-1    md:px-3  ">
+              <ul className="list-disc list-inside  mx-auto   px-[2rem] md:px-[3rem] text-xl space-y-8">
                 <li>An initial investment of £3,000 towards your licence fee (representing £1,500 per subject for maths and English)</li>
                 <li>You need to budget an additional £1,500 - £2,000 for marketing activity across your first 12 months of business</li>
                 <li>You need to allow for rent or hire of premises, as well as other minimal running costs</li>
@@ -308,8 +311,8 @@ const Frenchise = () => {
                 className="w-full h-auto"
               />
             </div>
-            <div className="md:w-2/3 mt-4 md:mt-0 md:ml-4">
-              <ul className="list-disc list-inside  px-[4rem]  text-xl  space-y-2">
+            <div className="md:w-2/3 mt-4 md:mt-0   ml-0  md:ml-4">
+              <ul className="list-disc list-inside px-[2rem] md:px-[4rem]  text-xl  space-y-2">
                 <li>You’ll set your own fees, typically in the region of £65 per subject, per month, which parents pay to you directly</li>
                 <li>You’ll be aiming to have average subject numbers of 150-200 subjects in your centre</li>
                 <li>You’ll pay Kumon a fixed cost royalty fee for each student/subject, so it’s easy to project your income</li>
@@ -323,6 +326,74 @@ const Frenchise = () => {
     </div>
 
     </div>
+
+
+
+
+
+
+
+
+
+
+
+{/* ======================part 4========================================== */}
+
+
+
+
+
+
+
+
+
+
+<div className="row-wrapper page-builder-bg-grey mt-[8rem] py-[15px]   ">
+  <div className="container-fluid">
+    <div className="row">
+      <div className="col col-sm-12">
+        <div className="wysiwyg_widget    text-l   mt-5   ">
+          <h2 className="text-center font-[400] text-[2rem] md:text-[3rem]">How much will it cost?</h2>
+        </div>                         
+             </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+<div className="row-wrapper ">
+  <div className="  w-[80vw] mx-auto   ">
+    <div className=" grid  grid-cols-1   lg:grid-cols-3   ">
+      <div className="lg:col-span-2">
+        <div className="wysiwyg_widget    text-lg leading-9  px-1 lg:px-5">
+          <p>The Kumon Institute of Education was founded in 1958 in Japan by Toru Kumon, both a parent and gifted maths teacher. He created what is now the Kumon Maths Programme for his son Takeshi to help him improve his ability in maths. Our programmes now develop English skills, as well as maths, and are constantly evolving, learning from students and Instructors, to make them as effective as possible. Kumon still stands firm to Toru Kumon’s founding principle: to help as many children as possible excel academically and realise their potential.<br />
+            
+            The Kumon Maths and English Programmes are complementary to work covered in school, building key skills and academic ability at the student’s own pace. Whilst Kumon does not follow any particular school curriculum, it does help students to build strong foundations that can help them to excel in all subjects at school.
+            <br />
+            &nbsp;
+            
+            
+            <div className="inline-block px-6 py-2 mt-4    my-[2rem] text-white bg-green-500 hover:bg-green-600 rounded">
+                  See History of Kumon <i className="fa fa-chevron-right" />
+                </div>
+            
+            </p>
+        </div>                              </div>
+      <div className="    w-full    h-auto md:w-[300px]  md:h-[300px]  lg:w-[420px]  lg:h-[400px] " >
+        <img src={kumon} alt="What is Kumon?"className="w-full h-full     justify-center  "  />
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
 
 
 
