63cfc3010cf164db62ffa46e4e0fcdbdf707c3c9
medcloud <medcloud.pk@gmail.com>
2024-07-03 00:07:25 +0500

    commit

--- server/server.js
+++ server/server.js
@@ -1,78 +1,50 @@
-import  express from "express";
-import  colors  from "colors";
+import express from "express";
+import colors from "colors";
 import dotenv from 'dotenv';
 import morgan from "morgan";
 import connectDB from "./config/db.js";
 import cors from 'cors';
-import  authRoute  from './routes/authRoute.js';
-import formRoutes from './routes/formRoutes.js'
-import franchiseRoutes from './routes/franchiseRoutes.js'
-import homesampling from './routes/homesamplingRoutes.js'
-import JobsForm from './routes/jobsRoutes.js'
-// import path  from "path";
+import authRoute from './routes/authRoute.js';
+import formRoutes from './routes/formRoutes.js';
+import franchiseRoutes from './routes/franchiseRoutes.js';
+import homesamplingRoutes from './routes/homesamplingRoutes.js'; // Corrected import
+import jobsRoutes from './routes/jobsRoutes.js'; // Corrected import
+
 const app = express();
 
-//configure env
+// Configure env
 dotenv.config();
 
-//database config
+// Database config
 connectDB();
 
-
-
-
-//middleware
-
-
-
-// Use the CORS middleware
+// Middleware
 app.use(cors({
-  origin: 'https://ldclab.vercel.app', // Allow only localhost:3000
+  origin: 'https://ldclab.vercel.app', // Allow only ldclab.vercel.app
   credentials: true, // Include credentials
 }));
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://ldclab.vercel.app");
-  next();
-});
-
-
-
-
-
 app.use(express.json());
 app.use(morgan('dev'));
-// app._router(express.static(path.join(__dirname,'./client/build')))
 
-//routes
 // Serve uploaded images statically
 app.use('/uploads', express.static('uploads'));
 
-
-// Serve PDF files staticallyapp.use(express.static(path.join(__dirname, 'ldc-project', 'build')));
-
-
+// Routes
 app.use("/api/v1/auth", authRoute);
 app.use('/api/sampleBooking', formRoutes);
 app.use('/api', franchiseRoutes);
-app.use('/api',homesampling)
-app.use('/api',JobsForm)
+app.use('/api', homesamplingRoutes); // Corrected usage
+app.use('/api', jobsRoutes); // Corrected usage
 
 app.get("/", (req, res) => {
-    res.send("server is running ");
-  });
+  res.send("Server is running");
+});
 
+// Port
+const PORT = process.env.PORT || 5000; // Added default port
 
-
-  
-
-//port
-const PORT = process.env.PORT ;
-
-
-//run listen
-
-app.listen(PORT, () =>{
-    console.log(`server is runing on ${PORT}`.bgCyan.white)
-})
-
+// Run server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`.cyan.bgWhite);
+});
