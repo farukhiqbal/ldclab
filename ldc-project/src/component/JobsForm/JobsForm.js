3e560f2b97197bd8592a8befeb58bcc77b9f437f
medcloud <medcloud.pk@gmail.com>
2024-07-02 16:04:44 +0500

    final

--- ldc-project/src/component/JobsForm/JobsForm.js
+++ ldc-project/src/component/JobsForm/JobsForm.js
@@ -41,7 +41,7 @@ const JobsForm = () => {
     <Layout>
       <div className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
         <div className="bg-white p-8 rounded shadow-md w-full max-w-4xl">
-          <div className="flex gap-[3rem] mb-[3rem] items-center">
+          <div className="flex    gap-[1rem]   md:gap-[1.5rem]   lg:gap-[2rem]   xl:gap-[3rem] mb-[3rem] items-center">
             <QRCode value="https://ldclab.vercel.app/job-form" className="mb-4" size={qrCodeSize} />
             <h2 className="text-[1.5rem] md:text-[2rem] lg:text-[3rem] font-bold mb-6 text-center">Job Application Form</h2>
           </div>
